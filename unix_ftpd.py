57e48fe153fee4748a3106b8e2464c5bc13340b9
Giampaolo Rodola <g.rodola@gmail.com>
2007-07-17 20:57:25 +0000

    Adding 0.1.0 release to /tag directory.

---
+++ unix_ftpd.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# unix_ftpd.py
+
+# FTPd using local unix account database to authenticate users and get
+# their home directories (users must be created previously).
+
+from pyftpdlib import FTPServer
+import os
+import pwd, spwd, crypt
+
+class unix_authorizer(FTPServer.dummy_authorizer):
+
+    def __init__(self):
+        FTPServer.dummy_authorizer.__init__(self)
+
+    def add_user(self, username, home='', perm=('r')):
+        assert username in [i[0] for i in pwd.getpwall()], 'No such user "%s".' %username
+        pw = spwd.getspnam(username).sp_pwd
+        if not home:
+            home = pwd.getpwnam(username).pw_dir
+        assert os.path.isdir(home), 'No such directory "%s".' %home
+        dic = {'pwd'  : pw,
+               'home' : home,
+               'perm' : perm
+               }
+        self.user_table[username] = dic
+
+    def validate_authentication(self, username, password):
+        if username == 'anonymous':
+            if self.has_user('anonymous'):
+                return 1
+        else:
+            pw1 = spwd.getspnam(username).sp_pwd
+            pw2 = crypt.crypt(password, pw1)
+            return pw1 == pw2
+
+if __name__ == "__main__":
+    authorizer = unix_authorizer()
+    authorizer.add_user('user', perm=('r', 'w'))
+    ftp_handler = FTPServer.ftp_handler
+    ftp_handler.authorizer = authorizer
+    address = ('', 21)
+    ftpd = FTPServer.ftp_server(address, ftp_handler)
+    ftpd.serve_forever()
