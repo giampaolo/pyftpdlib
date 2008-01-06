dedb0084490ef5d83f4831e37eafcc6f53abf351
Giampaolo Rodola <g.rodola@gmail.com>
2008-01-06 01:31:20 +0000

    Improved comments for STOU command.

--- pyftpdlib/ftpserver.py
+++ pyftpdlib/ftpserver.py
@@ -2069,8 +2069,13 @@ class FTPHandler(asynchat.async_chat):
                      %self.fs.ftpnorm(line))
             self.respond("550 Can't STOU: not enough privileges.")
             return
-
-        # now just acts like STOR except that restarting isn't allowed
+        
+        # ...Now we just acts like STOR except that restarting isn't
+        # allowed.
+        # - The 250 response code required in RFC-959 has been declared
+        # incorrect in RFC-1123 which requires 125/150 instead.
+        # - The exact format of the STOU response is hereby defined to
+        # be as "125/150 FILE: <filename>".
         log = 'OK STOU "%s". Upload starting.' %filename
         if self.data_channel:
             self.respond("125 FILE: %s" %filename)
