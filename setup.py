fe7a9a64c8624956ee710641a7e51dac042f0785
Giampaolo Rodola <g.rodola@gmail.com>
2008-01-16 18:11:59 +0000

    Update setup.py to fit with new 0.3.0 release.

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 # setup.py
 
+"""pyftpdlib installer.
+
+To install pyftpdlib just open a command shell and run:
+> python setup.py install
+"""
+
 from distutils.core import setup
 
-long_descr = """
+long_descr = """\
 Python FTP server library, based on asyncore framework, provides
 an high-level portable interface to easily write asynchronous
 FTP servers with Python."""
 
 setup(
     name='pyftpdlib',
-    version = "0.2.0",
-    description = 'High-level asynchronous FTP server library',
-    long_description = long_descr,
-    license = 'MIT License',
-    platforms = 'Platform Independent',
-    author = "Giampaolo Rodola'",
-    author_email = 'g.rodola@gmail.com',
-    url = 'http://code.google.com/p/pyftpdlib/',
-    download_url = 'http://code.google.com/p/pyftpdlib/downloads/list',
-    packages = ['pyftpdlib'],
-    classifiers = [
+    version="0.3.0",
+    description='High-level asynchronous FTP server library',
+    long_description=long_descr,
+    license='MIT License',
+    platforms='Platform Independent',
+    author="Giampaolo Rodola'",
+    author_email='g.rodola@gmail.com',
+    url='http://code.google.com/p/pyftpdlib/',
+    download_url='http://pyftpdlib.googlecode.com/files/pyftpdlib-0.3.0.tar.gz',
+    packages=['pyftpdlib'],
+    classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
           'Intended Audience :: Developers',
@@ -28,6 +34,7 @@ setup(
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
-          'Topic :: Internet :: File Transfer Protocol (FTP)'
+          'Topic :: Internet :: File Transfer Protocol (FTP)',
+          'Topic :: Software Development :: Libraries :: Python Modules'
           ],
     )
