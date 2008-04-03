a5ff5d088bb1a36aa07f5636916245f9be67190d
Giampaolo Rodola <g.rodola@gmail.com>
2008-04-03 19:39:04 +0000

    Fix issue #66: have WinNtAuthorizer determine the real user profile directory

--- demo/winnt_ftpd.py
+++ demo/winnt_ftpd.py
@@ -6,9 +6,24 @@
 """
 
 import os
-import win32security, win32net, win32profile, pywintypes
+import win32security, win32net, pywintypes
 
 from pyftpdlib import ftpserver
+
+
+def get_profile_dir(username):
+    """Return the user's profile directory."""
+    import _winreg, win32api
+    sid = win32security.ConvertSidToStringSid(
+            win32security.LookupAccountName(None, username)[0])
+    try:
+        key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,
+          r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"+"\\"+sid)
+    except WindowsError:
+        raise ftpserver.AuthorizerError("No profile directory defined for %s "
+                                        "user" %username)
+    value = _winreg.QueryValueEx(key, "ProfileImagePath")[0]
+    return win32api.ExpandEnvironmentStrings(value)
 
 
 class WinNtAuthorizer(ftpserver.DummyAuthorizer):
@@ -27,8 +42,7 @@ class WinNtAuthorizer(ftpserver.DummyAuthorizer):
         if not username in users:
             raise ftpserver.AuthorizerError('No such user "%s".' %username)
         if not home:
-            profiles = win32profile.GetProfilesDirectory()
-            home = str(os.path.join(profiles, username))
+            home = get_profile_dir(username)
         ftpserver.DummyAuthorizer.add_user(self, username, '', home, **kwargs)
 
     def validate_authentication(self, username, password):
