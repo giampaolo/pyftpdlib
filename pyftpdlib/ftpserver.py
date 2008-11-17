669adcfda4505a2fd1d4bd0299bccab1b2ba0743
Giampaolo Rodola <g.rodola@gmail.com>
2008-11-17 00:32:51 +0000

    Fix Issue #91: handled the case when sometimes, for some reason, accept() returns None instead of a socket.

--- pyftpdlib/ftpserver.py
+++ pyftpdlib/ftpserver.py
@@ -607,7 +607,12 @@ class PassiveDTP(asyncore.dispatcher):
 
     def handle_accept(self):
         """Called when remote client initiates a connection."""
-        sock, addr = self.accept()
+        try:
+            sock, addr = self.accept()
+        except TypeError:
+            # for some reason sometimes accept() returns None instead
+            # of a socket
+            return
         # Check the origin of data connection.  If not expressively
         # configured we drop the incoming data connection if remote
         # IP address does not match the client's IP address.
@@ -3069,7 +3074,12 @@ class FTPServer(asyncore.dispatcher):
 
     def handle_accept(self):
         """Called when remote client initiates a connection."""
-        sock_obj, addr = self.accept()
+        try:
+            sock_obj, addr = self.accept()
+        except TypeError:
+            # for some reason sometimes accept() returns None instead
+            # of a socket
+            return
         log("[]%s:%s Connected." %addr[:2])
 
         handler = self.handler(sock_obj, self)
