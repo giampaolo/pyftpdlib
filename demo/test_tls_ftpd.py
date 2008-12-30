978e9964420397d3ac37971199ada9f34c93c00d
Giampaolo Rodola <g.rodola@gmail.com>
2008-12-30 17:40:43 +0000

    Add temporary tls_ftpd test suite, hoping I won't forget to remove it before packaging.

---
+++ demo/test_tls_ftpd.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# test_tls_ftpd.py
+
+"""Minimal test suite for tls_ftpd.py script"""
+
+import unittest
+import ftplib
+import ssl
+import os
+import threading
+import StringIO
+
+import tls_ftpd
+from pyftpdlib import ftpserver
+
+
+HOST = 'localhost'
+USER = 'user'
+PASSWD = 'passwd'
+HOME = os.getcwd()
+try:
+    from test.test_support import TESTFN
+except ImportError:
+    TESTFN = 'temp-fname'
+
+
+class TLS_FTP(ftplib.FTP):
+    """A ftplib.FTP subclass which adds TLS support to FTP as described
+    in RFC-4217.
+
+    Connect as usual to port 21 securing control connection before
+    authenticating.
+
+    Usage example:
+    >>> import TLS_FTP
+    >>> ftps = TLS_FTP('ftp.python.org')
+    >>> ftps.login()  # login anonimously
+    '230 Guest login ok, access restrictions apply.'
+    >>> ftps.prot_p()  # switch to secure data connection
+    '200 Protection level set to P'
+    >>> ftps.retrlines('LIST')  # list directory content securely
+    total 9
+    drwxr-xr-x   8 root     wheel        1024 Jan  3  1994 .
+    drwxr-xr-x   8 root     wheel        1024 Jan  3  1994 ..
+    drwxr-xr-x   2 root     wheel        1024 Jan  3  1994 bin
+    drwxr-xr-x   2 root     wheel        1024 Jan  3  1994 etc
+    d-wxrwxr-x   2 ftp      wheel        1024 Sep  5 13:43 incoming
+    drwxr-xr-x   2 root     wheel        1024 Nov 17  1993 lib
+    drwxr-xr-x   6 1094     wheel        1024 Sep 13 19:07 pub
+    drwxr-xr-x   3 root     wheel        1024 Jan  3  1994 usr
+    -rw-r--r--   1 root     root          312 Aug  1  1994 welcome.msg
+    '226 Transfer complete.'
+    >>> ftps.quit()
+    '221 Goodbye.'
+    >>>
+    """
+
+    def __init__(self, host='', user='', passwd='', acct='', keyfile=None,
+                 certfile=None, timeout=None):
+        self.keyfile = keyfile
+        self.certfile = certfile
+        self.encrypted_pi = False
+        self.prot_private = False
+        ftplib.FTP.__init__(self, host, user, passwd, acct, timeout)
+
+    def auth_tls(self):
+        """Set up secure control connection by using TLS."""
+        resp = self.voidcmd('AUTH TLS')
+        self.sock = ssl.wrap_socket(self.sock, self.keyfile, self.certfile,
+                                    ssl_version=ssl.PROTOCOL_TLSv1)
+        self.file = self.sock.makefile(mode='rb')
+        return resp
+
+    def prot_p(self):
+        """Set up secure data connection."""
+        # PROT defines whether or not the data channel is to be protected.
+        # Though RFC-2228 defines four possible protection levels,
+        # RFC-4217 only recommends two, Clear and Private.
+        # Clear (PROT C) means that no security is to be used on the
+        # data-channel, Private (PROT P) means that the data-channel
+        # should be protected by TLS.
+        # PBSZ command MUST still be issued, but must have a parameter of
+        # '0' to indicate that no buffering is taking place and the data
+        # connection should not be encapsulated.
+        self.voidcmd('PBSZ 0')
+        resp = self.voidcmd('PROT P')
+        self.prot_private = True
+        return resp
+
+    def prot_c(self):
+        """Set up clear text data channel."""
+        resp = self.voidcmd('PROT C')
+        self.prot_private = False
+        return resp
+
+    def ntransfercmd(self, cmd, rest=None):
+        conn, size = ftplib.FTP.ntransfercmd(self, cmd, rest)
+        if self.prot_private:
+            conn = ssl.wrap_socket(conn, self.keyfile, self.certfile)
+        return conn, size
+
+    # overridden to accept 6xy as valid response code
+    def getresp(self):
+        resp = self.getmultiline()
+        if self.debugging: print '*resp*', self.sanitize(resp)
+        self.lastresp = resp[:3]
+        c = resp[:1]
+        if c in ('1', '2', '3', '6'):
+            return resp
+        if c == '4':
+            raise ftplib.error_temp, resp
+        if c == '5':
+            raise ftplib.error_perm, resp
+        raise ftplib.error_proto, resp
+
+
+class TLS_FTPd(threading.Thread):
+    """A threaded FTPS server used for running tests.
+
+    This is basically a modified version of the FTPServer class which
+    wraps the polling loop into a thread.
+
+    The instance returned can be used to start(), stop() and
+    eventually re-start() the server.
+    """
+
+    def __init__(self, host=HOST, port=0, verbose=False):
+        threading.Thread.__init__(self)
+        self.__serving = False
+        self.__stopped = False
+        self.__lock = threading.Lock()
+        self.__flag = threading.Event()
+
+        if not verbose:
+            ftpserver.log = ftpserver.logline = lambda x: x
+        self.authorizer = ftpserver.DummyAuthorizer()
+        self.authorizer.add_user(USER, PASSWD, HOME, perm='elradfmw')  # full perms
+        self.authorizer.add_anonymous(HOME)
+        self.handler = tls_ftpd.TLS_FTPHandler
+        self.handler.authorizer = self.authorizer
+        self.server = ftpserver.FTPServer((host, port), self.handler)
+        self.host, self.port = self.server.socket.getsockname()[:2]
+
+    def __repr__(self):
+        status = [self.__class__.__module__ + "." + self.__class__.__name__]
+        if self.__serving:
+            status.append('active')
+        else:
+            status.append('inactive')
+        status.append('%s:%s' %self.server.socket.getsockname()[:2])
+        return '<%s at %#x>' % (' '.join(status), id(self))
+
+    def start(self, timeout=0.001, use_poll=False, map=None):
+        """Start serving until an explicit stop() request.
+        Polls for shutdown every 'timeout' seconds.
+        """
+        if self.__serving:
+            raise RuntimeError("Server already started")
+        if self.__stopped:
+            # ensure the server can be started again
+            ThreadedFTPServer.__init__(self, self.socket.getsockname(),
+                                       self.handler)
+        self.__timeout = timeout
+        self.__use_poll = use_poll
+        self.__map = map
+        threading.Thread.start(self)
+        self.__flag.wait()
+
+    def run(self):
+        self.__serving = True
+        self.__flag.set()
+        while self.__serving:
+            self.__lock.acquire()
+            self.server.serve_forever(timeout=self.__timeout, count=1,
+                                      use_poll=self.__use_poll, map=self.__map)
+            self.__lock.release()
+        self.server.close_all(ignore_all=True)
+
+    def stop(self):
+        """Stop serving (also disconnecting all currently connected
+        clients) by telling the serve_forever() loop to stop and
+        waits until it does.
+        """
+        if not self.__serving:
+            raise RuntimeError("Server not started yet")
+        self.__serving = False
+        self.__stopped = True
+        self.join()
+
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.server = TLS_FTPd()
+        self.server.start()
+        self.client = TLS_FTP()
+        self.client.connect(self.server.host, self.server.port)
+        self.client.sock.settimeout(2)
+        self.file = open(TESTFN, 'w+b')
+        self.dummyfile = StringIO.StringIO()
+
+    def tearDown(self):
+        self.client.close()
+        self.server.stop()
+        if not self.file.closed:
+            self.file.close()
+        if not self.dummyfile.closed:
+            self.dummyfile.close()
+        os.remove(TESTFN)
+
+    def test_auth(self):
+        pass
+
+    def test_pbsz(self):
+        pass
+
+    def test_prot(self):
+        pass
+
+    def test_cleartext_data_transfer(self):
+        pass
+
+    def test_encrypted_data_transfer(self):
+        pass
+
+
+if __name__ == '__main__':
+    test_suite = unittest.TestSuite()
+    tests = [TestCase]
+    for test in tests:
+        test_suite.addTest(unittest.makeSuite(test))
+    unittest.TextTestRunner(verbosity=2).run(test_suite)
