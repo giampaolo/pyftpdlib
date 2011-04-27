2a9eca83babe93827cc1aff4512b5d7342acc9cd
Giampaolo Rodola <g.rodola@gmail.com>
2011-04-27 19:18:27 +0000

    modify tls_ftpd.py demo script so that it retrieves the certfile no matter what the current directory is

--- demo/tls_ftpd.py
+++ demo/tls_ftpd.py
@@ -6,16 +6,20 @@
 Requires PyOpenSSL module (http://pypi.python.org/pypi/pyOpenSSL).
 """
 
+import os
+
 from pyftpdlib import ftpserver
 from pyftpdlib.contrib.handlers import TLS_FTPHandler
 
+CERTFILE = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        "keycert.pem"))
 
 def main():
     authorizer = ftpserver.DummyAuthorizer()
     authorizer.add_user('user', '12345', '.', perm='elradfmw')
     authorizer.add_anonymous('.')
     ftp_handler = TLS_FTPHandler
-    ftp_handler.certfile = 'demo/keycert.pem'
+    ftp_handler.certfile = CERTFILE
     ftp_handler.authorizer = authorizer
     # requires SSL for both control and data channel
     #ftp_handler.tls_control_required = True
