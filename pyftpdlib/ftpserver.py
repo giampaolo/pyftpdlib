be9466ace7ea09f8ad21e411589872423898b83d
Giampaolo Rodola <g.rodola@gmail.com>
2012-11-16 18:46:40 +0000

    fix cmdline parser

--- pyftpdlib/ftpserver.py
+++ pyftpdlib/ftpserver.py
@@ -40,6 +40,7 @@ from pyftpdlib.servers import FTPServer, ...
 
 import logging
 
+from pyftpdlib import __ver__
 from pyftpdlib.handlers import *
 from pyftpdlib.authorizers import *
 from pyftpdlib.servers import *
@@ -80,79 +81,6 @@ def logerror(msg):
     _depwarn("pyftpdlib.ftpserver.logline() is deprecated")
     logging.error(msg)
 
-def main():
-    """Start a stand alone anonymous FTP server."""
-    import optparse
-    import sys
-    import os
-
-    from pyftpdlib.authorizers import DummyAuthorizer
-    from pyftpdlib.handlers import FTPHandler
-    from pyftpdlib.servers import FTPServer
-    from pyftpdlib._compat import getcwdu
-
-    class CustomizedOptionFormatter(optparse.IndentedHelpFormatter):
-        """Formats options shown in help in a prettier way."""
-
-        def format_option(self, option):
-            result = []
-            opts = self.option_strings[option]
-            result.append('  %s\n' % opts)
-            if option.help:
-                help_text = '     %s\n\n' % self.expand_default(option)
-                result.append(help_text)
-            return ''.join(result)
-
-    usage = "python -m pyftpdlib.ftpserver [options]"
-    parser = optparse.OptionParser(usage=usage, description=main.__doc__,
-                                   formatter=CustomizedOptionFormatter())
-    parser.add_option('-i', '--interface', default='', metavar="ADDRESS",
-                      help="specify the interface to run on (default all "
-                           "interfaces)")
-    parser.add_option('-p', '--port', type="int", default=21, metavar="PORT",
-                      help="specity port number to run on (default 21)")
-    parser.add_option('-w', '--write', action="store_true", default=False,
-                      help="grants write access for the anonymous user "
-                           "(default read-only)")
-    parser.add_option('-d', '--directory', default=getcwdu(), metavar="FOLDER",
-                      help="specify the directory to share (default current "
-                           "directory)")
-    parser.add_option('-n', '--nat-address', default=None, metavar="ADDRESS",
-                      help="the NAT address to use for passive connections")
-    parser.add_option('-r', '--range', default=None, metavar="FROM-TO",
-                      help="the range of TCP ports to use for passive "
-                           "connections (e.g. -r 8000-9000)")
-    parser.add_option('-v', '--version', action='store_true',
-                      help="print pyftpdlib version and exit")
-
-    options, args = parser.parse_args()
-    if options.version:
-        sys.exit("pyftpdlib %s" % __ver__)
-    passive_ports = None
-    if options.range:
-        try:
-            start, stop = options.range.split('-')
-            start = int(start)
-            stop = int(stop)
-        except ValueError:
-            parser.error('invalid argument passed to -r option')
-        else:
-            passive_ports = list(range(start, stop + 1))
-    # On recent Windows versions, if address is not specified and IPv6
-    # is installed the socket will listen on IPv6 by default; in this
-    # case we force IPv4 instead.
-    if os.name in ('nt', 'ce') and not options.interface:
-        options.interface = '0.0.0.0'
-
-    authorizer = DummyAuthorizer()
-    perm = options.write and "elradfmwM" or "elr"
-    authorizer.add_anonymous(options.directory, perm=perm)
-    handler = FTPHandler
-    handler.authorizer = authorizer
-    handler.masquerade_address = options.nat_address
-    handler.passive_ports = passive_ports
-    ftpd = FTPServer((options.interface, options.port), FTPHandler)
-    ftpd.serve_forever()
-
 if __name__ == '__main__':
+    from pyftpdlib import main
     main()
