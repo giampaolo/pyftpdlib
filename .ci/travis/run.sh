c7b73b82b93b21fffdbc50c78377de71d75f6cc3
Giampaolo Rodola <g.rodola@gmail.com>
2015-10-31 18:48:07 +0100

    fix travis

--- .ci/travis/run.sh
+++ .ci/travis/run.sh
@@ -7,10 +7,10 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
-    pyenv activate pyftpdlib mock
+    pyenv activate pyftpdlib
 fi
 
-pip install flake8 pyopenssl
+pip install flake8 pyopenssl mock
 python setup.py install
 python pyftpdlib/test/runner.py
 flake8
