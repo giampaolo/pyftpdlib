3d61dd311eddd39ef05e27d120229ef647bf3dfc
Giampaolo Rodola <g.rodola@gmail.com>
2016-02-26 10:41:35 +0100

    try to fix travis

--- .ci/travis/install.sh
+++ .ci/travis/install.sh
@@ -1,8 +1,10 @@
-    esac
 #!/bin/bash
 
 set -e
 set -x
+
+uname -a
+python -c "import sys; print(sys.version)"
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
@@ -14,29 +16,29 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     fi
 
     case "${PYVER}" in
-        py26)
-            pyenv install 2.6.9
-            pyenv virtualenv 2.6.9 psutil
-            ;;
+        # py26)
+        #     pyenv install 2.6.9
+        #     pyenv virtualenv 2.6.9 psutil
+        #     ;;
         py27)
             pyenv install 2.7.10
             pyenv virtualenv 2.7.10 psutil
             ;;
-        py32)
-            pyenv install 3.2.6
-            pyenv virtualenv 3.2.6 psutil
-            ;;
-        py33)
-            pyenv install 3.3.6
-            pyenv virtualenv 3.3.6 psutil
-            ;;
+        # py32)
+        #     pyenv install 3.2.6
+        #     pyenv virtualenv 3.2.6 psutil
+        #     ;;
+        # py33)
+        #     pyenv install 3.3.6
+        #     pyenv virtualenv 3.3.6 psutil
+        #     ;;
         py34)
             pyenv install 3.4.3
             pyenv virtualenv 3.4.3 psutil
             ;;
     esac
     pyenv rehash
-    pyenv activate pyftpdlib
+    pyenv activate psutil
 fi
 
 if [[ $TRAVIS_PYTHON_VERSION == '2.6' ]] || [[ $PYVER == 'py26' ]]; then
