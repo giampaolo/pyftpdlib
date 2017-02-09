ab699b5f89223e03593f3e004d6a370b4c2e5308
Giampaolo Rodola <g.rodola@gmail.com>
2017-02-09 11:17:22 +0100

    update copyright banner

--- demo/tls_ftpd.py
+++ demo/tls_ftpd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2007-2016 Giampaolo Rodola' <g.rodola@gmail.com>.
+# Copyright (C) 2007 Giampaolo Rodola' <g.rodola@gmail.com>.
 # Use of this source code is governed by MIT license that can be
 # found in the LICENSE file.
 
--- pyftpdlib/__init__.py
+++ pyftpdlib/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2007-2016 Giampaolo Rodola' <g.rodola@gmail.com>.
+# Copyright (C) 2007 Giampaolo Rodola' <g.rodola@gmail.com>.
 # Use of this source code is governed by MIT license that can be
 # found in the LICENSE file.
 
