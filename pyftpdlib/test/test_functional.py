c972060096fb99b5f6b932d4fb8a591b3259871c
Giampaolo Rodola <g.rodola@gmail.com>
2017-10-13 13:04:30 +0200

    move cmdline tests in test_misc

--- pyftpdlib/test/test_functional.py
+++ pyftpdlib/test/test_functional.py
@@ -62,7 +62,6 @@ from pyftpdlib.test import unittest
 from pyftpdlib.test import USER
 from pyftpdlib.test import VERBOSITY
 from pyftpdlib.test import WINDOWS
-import pyftpdlib.__main__
 
 try:
     from StringIO import StringIO as BytesIO
@@ -2758,128 +2757,6 @@ class TestUnicodePathNames(unittest.TestCase):
             dummy = BytesIO()
             self.assertRaises(ftplib.error_perm, self.client.retrbinary,
                               'retr ' + TESTFN_UNICODE_2, dummy.write)
-
-
-class TestCommandLineParser(unittest.TestCase):
-    """Test command line parser."""
-    SYSARGV = sys.argv
-    STDERR = sys.stderr
-
-    def setUp(self):
-        class DummyFTPServer(FTPServer):
-            """An overridden version of FTPServer class which forces
-            serve_forever() to return immediately.
-            """
-
-            def serve_forever(self, *args, **kwargs):
-                return
-
-        if PY3:
-            import io
-            self.devnull = io.StringIO()
-        else:
-            self.devnull = BytesIO()
-        sys.argv = self.SYSARGV[:]
-        sys.stderr = self.STDERR
-        self.original_ftpserver_class = FTPServer
-        pyftpdlib.__main__.FTPServer = DummyFTPServer
-
-    def tearDown(self):
-        self.devnull.close()
-        sys.argv = self.SYSARGV[:]
-        sys.stderr = self.STDERR
-        pyftpdlib.servers.FTPServer = self.original_ftpserver_class
-        safe_rmdir(TESTFN)
-
-    def test_a_option(self):
-        sys.argv += ["-i", "localhost", "-p", "0"]
-        pyftpdlib.__main__.main()
-        sys.argv = self.SYSARGV[:]
-
-        # no argument
-        sys.argv += ["-a"]
-        sys.stderr = self.devnull
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-    def test_p_option(self):
-        sys.argv += ["-p", "0"]
-        pyftpdlib.__main__.main()
-
-        # no argument
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-p"]
-        sys.stderr = self.devnull
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-        # invalid argument
-        sys.argv += ["-p foo"]
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-    def test_w_option(self):
-        sys.argv += ["-w", "-p", "0"]
-        with warnings.catch_warnings():
-            warnings.filterwarnings("error")
-            self.assertRaises(RuntimeWarning, pyftpdlib.__main__.main)
-
-        # unexpected argument
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-w foo"]
-        sys.stderr = self.devnull
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-    def test_d_option(self):
-        sys.argv += ["-d", TESTFN, "-p", "0"]
-        safe_mkdir(TESTFN)
-        pyftpdlib.__main__.main()
-
-        # without argument
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-d"]
-        sys.stderr = self.devnull
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-        # no such directory
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-d %s" % TESTFN]
-        safe_rmdir(TESTFN)
-        self.assertRaises(ValueError, pyftpdlib.__main__.main)
-
-    def test_r_option(self):
-        sys.argv += ["-r 60000-61000", "-p", "0"]
-        pyftpdlib.__main__.main()
-
-        # without arg
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-r"]
-        sys.stderr = self.devnull
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-        # wrong arg
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-r yyy-zzz"]
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-    def test_v_option(self):
-        sys.argv += ["-v"]
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-        # unexpected argument
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-v foo"]
-        sys.stderr = self.devnull
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
-
-    def test_D_option(self):
-        with mock.patch('pyftpdlib.__main__.config_logging') as fun:
-            sys.argv += ["-D", "-p 0"]
-            pyftpdlib.__main__.main()
-            fun.assert_called_once_with(level=logging.DEBUG)
-
-        # unexpected argument
-        sys.argv = self.SYSARGV[:]
-        sys.argv += ["-V foo"]
-        sys.stderr = self.devnull
-        self.assertRaises(SystemExit, pyftpdlib.__main__.main)
 
 
 configure_logging()
