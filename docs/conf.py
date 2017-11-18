a3215ebf88387a45ea7803f052e225a1d92e7845
Giampaolo Rodola <g.rodola@gmail.com>
2017-11-18 13:08:35 +0100

    remove doc files which are no longer used

--- docs/conf.py
+++ docs/conf.py
@@ -183,7 +183,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -372,6 +372,6 @@ html_context = {
     'css_files': [
         'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
         'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-        '_static/css/custom.css',
+        # '_static/css/custom.css',
     ],
 }
