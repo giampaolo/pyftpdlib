3b38ba4d3ca90e16cd2da06cb76abe760939b5c2
Giampaolo Rodola <g.rodola@gmail.com>
2024-09-16 00:49:34 +0200

    fix rst-check

--- docs/conf.py
+++ docs/conf.py
@@ -27,4 +27,3 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
