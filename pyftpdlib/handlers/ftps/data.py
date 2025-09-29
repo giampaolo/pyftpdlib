b006d2bb5a54aa333d9762a32caa0f81e313c601
Giampaolo Rodola <g.rodola@gmail.com>
2025-09-29 16:48:44 +0200

    Adjust imports

--- pyftpdlib/handlers/ftps/data.py
+++ pyftpdlib/handlers/ftps/data.py
@@ -4,34 +4,33 @@
 
 
 from pyftpdlib.handlers.ftp.data import DTPHandler
-from pyftpdlib.utils import has_ssl
 from pyftpdlib.utils import is_ssl_sock
 
-if has_ssl():
-    from .ssl import SSLConnectionMixin
+from .ssl import SSLConnectionMixin
 
-    class TLS_DTPHandler(SSLConnectionMixin, DTPHandler):
-        """A DTPHandler subclass supporting TLS/SSL."""
 
-        def __init__(self, sock, cmd_channel):
-            super().__init__(sock, cmd_channel)
-            if self.cmd_channel._prot:
-                self.secure_connection(self.cmd_channel.ssl_context)
+class TLS_DTPHandler(SSLConnectionMixin, DTPHandler):
+    """A DTPHandler subclass supporting TLS/SSL."""
 
-        def __repr__(self):
-            return DTPHandler.__repr__(self)
+    def __init__(self, sock, cmd_channel):
+        super().__init__(sock, cmd_channel)
+        if self.cmd_channel._prot:
+            self.secure_connection(self.cmd_channel.ssl_context)
 
-        def use_sendfile(self):
-            if is_ssl_sock(self.socket):
-                return False
-            else:
-                return super().use_sendfile()
+    def __repr__(self):
+        return DTPHandler.__repr__(self)
 
-        def handle_failed_ssl_handshake(self):
-            # TLS/SSL handshake failure, probably client's fault which
-            # used a SSL version different from server's.
-            # RFC-4217, chapter 10.2 expects us to return 522 over the
-            # command channel.
-            self.cmd_channel.respond("522 SSL handshake failed.")
-            self.cmd_channel.log_cmd("PROT", "P", 522, "SSL handshake failed.")
-            self.close()
+    def use_sendfile(self):
+        if is_ssl_sock(self.socket):
+            return False
+        else:
+            return super().use_sendfile()
+
+    def handle_failed_ssl_handshake(self):
+        # TLS/SSL handshake failure, probably client's fault which
+        # used a SSL version different from server's.
+        # RFC-4217, chapter 10.2 expects us to return 522 over the
+        # command channel.
+        self.cmd_channel.respond("522 SSL handshake failed.")
+        self.cmd_channel.log_cmd("PROT", "P", 522, "SSL handshake failed.")
+        self.close()
